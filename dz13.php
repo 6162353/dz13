e85279f730e404818dfad5bd1ff1cab613c135cb
6162353 <6162353@gmail.com>
2015-07-25 17:05:13 +0300

    done dz13

---
+++ dz13.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+  переделать 2 задание через JS
+ * 
+ * 
+ */
+$site_dir = '/dz13';
+
+echo '<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>dz13 js</title>
+
+
+
+
+</head>
+        <body>
+        
+        
+        
+<script src="' . $site_dir . '/main.js"></script>
+</body>
+        </html>';
+
+
+
